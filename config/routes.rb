ad51b5c3cc9bc40a6235c96fe43d34be0f13a5b1
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-09 18:09:51 +0100

    ajout des routes pour editer le model user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-   get 'users/index'
-   get 'users/show'
-   get 'users/new'
-   get 'users/edit'
-   get 'users/create'
-   get 'users/update'
-   get 'users/destroy'
    devise_for :users
+   resources :users, only: [:show, :edit, :update]
    resources :engines
    root to: 'home#index'
    
