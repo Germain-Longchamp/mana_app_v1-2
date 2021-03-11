0d84fe93e0926a93b3aa6a32254452f98dca42a7
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-11 09:33:50 +0100

    creation du model tagging qui va permettre de relier les tags aux engines belong_to tag et belongs_to engin

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_082805) do
+ActiveRecord::Schema.define(version: 2021_03_11_083228) do
 
   create_table "engines", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "engine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["engine_id"], name: "index_taggings_on_engine_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -41,4 +50,6 @@ ActiveRecord::Schema.define(version: 2021_03_11_082805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "taggings", "engines"
+  add_foreign_key "taggings", "tags"
 end
