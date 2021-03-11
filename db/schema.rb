a442cc6423cacc356e8c5317856d5199d5a9fdca
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-11 09:30:55 +0100

    creation du model tag avec migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_183455) do
+ActiveRecord::Schema.define(version: 2021_03_11_082805) do
 
   create_table "engines", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
