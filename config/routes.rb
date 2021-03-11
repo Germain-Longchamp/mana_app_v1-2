2baa7e7f2bd735ab1877d3af6fc6ea2bc471db79
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-11 09:59:24 +0100

    modification des vues pour ajouter les familles a chaque engine et creation de la route pour avoir les engines par tag

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
    devise_for :users
    resources :users, only: [:show, :edit, :update]
    resources :engines
+
+   # link tags to engines with the respective tag
+   get 'tags/:tag', to: 'posts#index', as: :tag
+
    root to: 'home#index'
    
 end
