7f082246c7cd869d140b3de2da57ed59af205801
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-14 13:33:19 +0100

    ajout des routes pour creer un tag

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
    get 'tags/:tag', to: 'engines#index', as: :tag
    get 'tags', to: 'tags#index'
 
+   get 'cars/new', to: 'cars#new'
+   post 'cars', to: 'cars#create'
    delete 'tags/:id', to: 'tags#destroy'
 
    root to: 'home#index'
