b0c901597fd2acc605f3bdd846dcf65d6de16954
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-14 17:41:39 +0100

    ajout de la route settings pour préparer la partie paramétrages des plateformes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,29 @@
 Rails.application.routes.draw do
 
+   # Routes for users
    devise_for :users
    resources :users, only: [:show, :edit, :update]
-   resources :engines
 
+   # Routes for engines
+   resources :engines
 
    # link tags to engines with the respective tag
    get 'tags/:tag', to: 'engines#index', as: :tag
+   
+   # Tags /index /create /new /destroy
    get 'tags', to: 'tags#index'
-
    post 'tags', to: 'tags#create'
    get 'tag/new', to: 'tags#new'
-   
    delete 'tags/:id', to: 'tags#destroy'
 
-
+   # About pages
    get 'about', to: 'about#index'
    get 'about/founders', to: 'about#founders'
 
+   # Settings pages
+   get 'settings', to: 'settings#index'
+
+   # Home root
    root to: 'home#index'
    
 end
