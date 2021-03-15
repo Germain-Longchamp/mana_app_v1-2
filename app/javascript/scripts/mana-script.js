0c0b55e6cad7d09dee8a4b3bacb1dce76b8b9344
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-15 13:46:28 +0100

    amelioration du click sur filtre tag et dynamisation du texte affiché

--- app/javascript/scripts/mana-script.js
+++ app/javascript/scripts/mana-script.js
@@ -1,24 +1,31 @@
 $(document).on('ready turbolinks:load', function() {
-    // Manage Device Notice Visibility
+    // ------------ Manage Device Notice Visibility
     var deviseMsg = $('#notice');
     var deviseBox = $('.devise-msg');
     var deviseClose = $('.devise-msg__close');
-
+    // Manage devise box notics
     if (deviseMsg.is(':empty')) {
         deviseBox.removeClass('displayed');
     } else {
         deviseBox.addClass('displayed');
     }
-
+    // Manage close button
     deviseClose.click(function() {
         deviseBox.hide();
     });
 
-    // Manage click on filter Engines Index
+    // ------------- Manage click on filter Engines Index
     var selectBtn = $('.filter-item-select__btn');
     var selectDrop = $('.filter-item-select__dropdown');
-
-    $(selectBtn).click(function() {
+    // Manage when click on button
+    $(selectBtn).click(function(e) {
         selectDrop.toggleClass('visible');
+        e.stopPropagation();
+    });
+    // Manage when click outside
+    $(document).on("click", function(e) {
+        if ($(e.target).is('.mod-engines-index__filter') === false) {
+            $(selectDrop).removeClass('visible');
+        }
     });
 });
