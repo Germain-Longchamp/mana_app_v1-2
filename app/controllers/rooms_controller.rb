5aefadf2da3bb11ef527a611ef1b6b603a552a06
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-16 13:34:26 +0100

    ajout du formulaire pour creer editer une salle et formatage index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,22 +1,57 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room, only: %i[ edit update destroy ]
+
   def index
+    @rooms = Room.order('name')
   end
 
   def show
   end
 
   def new
+    @room = Room.new
   end
 
   def edit
   end
 
   def create
+    @room = Room.new(room_params)
+    @room.save
+    redirect_to rooms_path
   end
 
   def update
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to rooms_url, notice: "La salle a été créée" }
+        format.json { render :show, status: :created, location: @room }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @room.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @room.destroy
+    respond_to do |format|
+      format.html { redirect_to rooms_url, notice: "La salle a bien été supprimée" }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
