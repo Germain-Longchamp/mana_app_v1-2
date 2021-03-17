f143d705946edeec1ff441bcc73068f25d702a62
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-17 17:23:14 +0100

    nouvelle navigation et ajout name et lastname forms user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :avatar)
+    params.require(:user).permit(:email, :password, :avatar, :firstname, :lastname)
   end
 end
