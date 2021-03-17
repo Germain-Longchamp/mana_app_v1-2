5398081689d57886c160d04b343eb7c42f91d819
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-17 16:47:55 +0100

    ajout des colonnes firstname et lastname user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_082215) do
+ActiveRecord::Schema.define(version: 2021_03_17_154703) do
 
   create_table "engines", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_03_16_082215) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
