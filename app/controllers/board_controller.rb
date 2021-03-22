c468247de2224931b82eb5ac012424ed3a48843e
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-22 22:38:52 +0100

    remontee des infos engines last updated sur board

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,5 +1,7 @@
 class BoardController < ApplicationController
 	def index
 		@engines = Engine.all
+		# GET LAST 10 UPDATED ENGINES
+		@engines_updated = Engine.order(:updated_at).reverse_order().limit(10)
 	end
 end
