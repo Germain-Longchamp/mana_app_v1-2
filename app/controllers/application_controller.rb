53e7cad70e9450eecdb8d978c2c5fde9cfd6f317
Germain-Longchamp <glongchamp8@gmail.com>
2021-03-29 09:06:23 +0200

    modification de la redirection pour board index apres sign in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+ protect_from_forgery with: :exception
+ before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+     #Override path after sign in
+     def after_sign_in_path_for(resource)
+          board_path
+     end
 
-    protected
+     protected
 
-      def configure_permitted_parameters
-           devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
-
-           devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :avatar, :firstname, :lastname, :position)}
-      end
+     def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
+          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :avatar, :firstname, :lastname, :position)}
+     end
 end
