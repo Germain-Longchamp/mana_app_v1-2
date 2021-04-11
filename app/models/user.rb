6e54587968019f52b619e94e825766d1e8856454
Germain-Longchamp <glongchamp8@gmail.com>
2021-04-11 18:41:34 +0200

    changement devise registration edit form par simple form for

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
+  
   has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
