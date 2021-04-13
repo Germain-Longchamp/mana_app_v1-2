6dacc234800ced031987277e038eba2545aa574e
Germain-Longchamp <glongchamp8@gmail.com>
2021-04-13 19:42:44 +0200

    mise en place du form pour créer un nouvel utilisateur appartenant à une boite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
      protected
 
      def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :firstname, :lastname, :company_id)}
           devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :avatar, :firstname, :lastname, :position, :company_id)}
      end
 end
