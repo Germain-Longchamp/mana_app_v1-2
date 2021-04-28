e777eb5b0da42674328b2206080fe0e21c271e17
Germain-Longchamp <glongchamp8@gmail.com>
2021-04-28 13:55:12 +0200

    essai dintegrer le calendrier en AJAX sur board index - en progression

--- app/helpers/engines_helper.rb
+++ app/helpers/engines_helper.rb
@@ -1,2 +1,9 @@
 module EnginesHelper
+    def events_ajax_previous_link
+        ->(param, date_range) { link_to, {param => date_range.first - 1.day }, remote: :true}
+    end
+
+    def events_ajax_next_link
+        ->(param, date_range) { link_to, {param => date_range.last + 1.day }, remote: :true}
+    end
 end
