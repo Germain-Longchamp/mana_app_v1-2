77b32273a24d61f4c5c96e7f8f2c75312d16b5b2
Germain-Longchamp <glongchamp8@gmail.com>
2021-04-28 17:57:30 +0200

    retour en arriere ajax simple calendar board index

--- app/helpers/engines_helper.rb
+++ app/helpers/engines_helper.rb
@@ -1,9 +1,3 @@
 module EnginesHelper
-    def events_ajax_previous_link
-        ->(param, date_range) { link_to, {param => date_range.first - 1.day }, remote: :true}
-    end
 
-    def events_ajax_next_link
-        ->(param, date_range) { link_to, {param => date_range.last + 1.day }, remote: :true}
-    end
 end
