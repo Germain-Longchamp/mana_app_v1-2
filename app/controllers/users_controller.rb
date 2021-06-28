e50ab0092aeff6930e656d8cc3592eedb2cce0d5
Germain-Longchamp <glongchamp8@gmail.com>
2021-06-28 09:50:10 +0200

    amelioration fonctionnement ajout user sur compte par attribution automatique de l'entreprise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # POST /engines or /engines.json
+  # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
+    @user.company_id = current_user.company_id
     @user.save
     redirect_to company_path(current_user.company_id)
   end
@@ -33,11 +33,11 @@ class UsersController < ApplicationController
 
   def profile
    @user = current_user
-  end
+ end
 
-  private
+ private
 
-  def set_user
+   def set_user
     @user = User.find(params[:id])
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable, :trackable
+         :recoverable, :rememberable, :validatable, :timeoutable
 
   belongs_to :company
   
