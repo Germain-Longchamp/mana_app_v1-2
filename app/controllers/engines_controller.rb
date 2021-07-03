dcd8f78613d9b1e900b5fcf8eb2f2a77d1bd05a5
Germain-Longchamp <glongchamp8@gmail.com>
2021-07-03 11:12:32 +0200

    ajout end validity date au form engine et au engine_params controller

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -91,6 +91,6 @@ class EnginesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def engine_params
-      params.require(:engine).permit(:name, :internal_name, :description, :tag_list, :tag, { tag_ids: [] }, :tag_ids, :room_id, :initial_date, :validity_start_date, :company_id)
+      params.require(:engine).permit(:name, :internal_name, :description, :tag_list, :tag, { tag_ids: [] }, :tag_ids, :room_id, :initial_date, :validity_start_date, :validity_end_date, :company_id)
     end
 end
