71e768d913a9452ff7ab5e9dd201ce663addef0c
Germain-Longchamp <glongchamp8@gmail.com>
2021-07-11 19:45:11 +0200

    creation animation ecrans sur creation compte

--- app/javascript/scripts/mana-script.js
+++ app/javascript/scripts/mana-script.js
@@ -26,7 +26,16 @@ $(document).on('ready turbolinks:load', function() {
             type: 'POST',
             url: '/companies',
             data: datas,
-            success: function(data) {},
+            success: function(data) {
+                $('#screenCompany').hide();
+                $('#screenWaiting').show();
+
+                setTimeout(
+                    function() {
+                        $('#screenWaiting').hide();
+                        $('#screenUser').show();
+                    }, 3000);
+            },
             error: function(response) {}
         });
 
