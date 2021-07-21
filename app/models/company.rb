d13f5098444aebcfc0b15460b65f20a17f64744a
Germain-Longchamp <glongchamp8@gmail.com>
2021-07-21 17:51:30 +0200

    correction model company pour delete toutes les dépendances a la suppression du compte

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
-    has_many :users
-    has_many :engines
-    has_many :tags
+    has_many :users, dependent: :delete_all
+    has_many :engines, dependent: :delete_all
+    has_many :tags, dependent: :delete_all
+    has_many :rooms, dependent: :delete_all
 end
