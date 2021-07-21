e4c3933f244df789fa6dd78f08e7580f63ec5795
Germain-Longchamp <glongchamp8@gmail.com>
2021-07-21 12:16:28 +0200

    ajout des prochaines verifications engine sur board index

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -5,6 +5,6 @@ class BoardController < ApplicationController
 		# GET LAST 50 UPDATED ENGINES
 		@engines_updated = Engine.where(:company_id => current_user.company_id).order(:updated_at).reverse_order().limit(50)
 		# GET 20 NEXT ENGINES TO UPDATE
-		@engines_next_update = Engine.where(:company_id => current_user.company_id).order(:validity_end_date).limit(20)
+		@engines_next_update = Engine.where(:company_id => current_user.company_id).limit(30).order(:validity_end_date)
 	end
 end
