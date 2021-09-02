cd1311192b4cf1a83973cb79f2b3e24471eeacfa
Germain-Longchamp <glongchamp8@gmail.com>
2021-09-02 15:21:10 +0200

    ajout bouton update status sur issue card

--- app/javascript/scripts/mana-script.js
+++ app/javascript/scripts/mana-script.js
@@ -1,5 +1,26 @@
 $(document).on('ready turbolinks:load', function() {
 
+    // ------------AJAX UPDATE ISSUE
+    $('.up-issue').click(function() {
+        var idIssue = $(this).attr('data-id');
+        var patchUrl = '/issues/' + idIssue;
+        var datas = [{
+            "name": "issue[id]",
+            "value": idIssue,
+        }, {
+            "name": "issue[status]",
+            "value": "resolu"
+        }];
+        $.ajax({
+            type: 'PATCH',
+            url: patchUrl,
+            data: datas,
+            success: function(data) {},
+            error: function(response) {}
+        });
+    });
+
+
     // ------------Close panel add tag
     $('.sidebar-adding__close').click(function() {
         $('#formTag').removeClass('slided');
