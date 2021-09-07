673720103a5b5ff54366965a5a21988d6451261f
Germain-Longchamp <glongchamp8@gmail.com>
2021-09-07 16:13:07 +0200

    esaai 4 config cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   
   has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
+  
   mount_uploader :picture, PictureUploader
 end
--- config/initializers/cloudinary.rb
+++
@@ -1,5 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = 'hwerhj1ii'
-    config.api_key = '629433635375844'
-    config.api_secret = 'F2GF2t8FN4rSpS6nA7f_sYRhIiI'
-end
